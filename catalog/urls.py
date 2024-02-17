6f118164e701ad2ba2452777c6f56bbd67dcb98a
zixeel <strpan549@gmail.com>
2024-02-18 02:58:43 +0300

    Недоделаное дз

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 from catalog.views import contacts, product, ProductListView, ProductDetailView, CategoryListView, \
-    BlogListView, main, BlogDetailView
+    BlogListView, main, BlogDetailView, BlogCreateView, BlogUpdateView
 
 app_name = CatalogConfig.name
 
@@ -14,7 +14,10 @@ urlpatterns = [
     path('category/', CategoryListView.as_view(), name='category'),
     path('category/<int:pk>/', product, name='product', ),
     path('view_product/<int:pk>', ProductDetailView.as_view(), name='view_product'),
-    path('blog/', BlogListView.as_view(), name='blog_list'),
+
+    path('blog_list/', BlogListView.as_view(), name='blog_list'),
     path('view_blog/<int:pk>', BlogDetailView.as_view(), name='view_blog'),
+    path('blog_create/', BlogCreateView.as_view(), name='create_blog'),
+    path('blog_edit/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 
 from catalog.models import Category, Product, Blog
 
@@ -51,7 +52,29 @@ class BlogDetailView(DetailView):
     model = Blog
 
     def get_object(self, queryset=None):
+        """счётчик просмотров"""
         self.object = super().get_object(queryset)
         self.object.view_count += 1
         self.object.save()
         return self.object
+
+
+class BlogCreateView(CreateView):
+    """создание блога"""
+    model = Blog
+    fields = ('title', 'content', 'preview', 'is_published')
+    success_url = reverse_lazy('catalog:blog_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'preview', 'is_published')
+    success_url = reverse_lazy('catalog:blog_list')
+    """
+     Пытался сделать в соотсветсвии с требованиями задания что бы редиректило на детальный просмотр того блога который
+     редактировали но мне выдаёт оишбку  
+     NoReverseMatch at /blog_edit/2/
+     Reverse for 'catalog/view_blog/' not found. 'catalog/view_blog/' is not a valid view function or pattern name.
+    """
+    # def get_success_url(self):
+    #     return reverse_lazy('view_blog/', kwargs={'pk': self.object.pk})
